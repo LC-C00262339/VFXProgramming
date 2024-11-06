6e933f9ddafee38931b6be547bd7f510b0a7e1a5
(Student  C00262339) Leo Carabini <c00262339@setu.ie>
2024-11-06 08:56:51 +0000

    General status update

--- practical_06/vfx_script.py
+++ practical_06/vfx_script.py
@@ -3,6 +3,15 @@ import sys
 import importlib
 from math import pi, radians
 
-bpy.ops.primitive_cube_add(location=(0,1,0))
-bpy.ops.primitive_cone_add(location=(0,2,0))
-bpy.ops.primitive_uv_sphere_add(segments=8, ring_count=8, radius=1.0, location=(0,3,0))
+# Test code
+# bpy.ops.primitive_cube_add(location=(0,1,0))
+# bpy.ops.primitive_cone_add(location=(0,2,0))
+# bpy.ops.primitive_uv_sphere_add(segments=8, ring_count=8, radius=1.0, location=(0,3,0))
+
+# Object one
+
+bpy.ops.primitive_cylinder_add(vertices=14, radius=1.0, depth=0.8)
+
+# Object two
+
+# Object three
