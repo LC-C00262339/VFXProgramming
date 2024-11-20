011dd2e1b65f44f90d4102d68e66f3067b2526b2
LC-C00262339 <c00262339@setu.ie>
2024-11-20 09:53:05 +0000

    Added scripting for adding cone

--- practical_07/mesh_script.py
+++ practical_07/mesh_script.py
@@ -3,3 +3,6 @@ import bpy
 bpy.ops.mesh.primitive_cylinder_add(location=(0, 0, 0), vertices=12) # Create cylinder
 cylinder = bpy.context.object
 cylinder.scale = (1, 1, 2)
+
+
+bpy.ops.mesh.primitive_cone_add(location=(0, 0, 4), vertices=12, radius1=1.5, radius2=0) # Create cone (roof)
