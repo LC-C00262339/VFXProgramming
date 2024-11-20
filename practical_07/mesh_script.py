6034701b7374cbc64d3929e3c306edaddf925735
LC-C00262339 <c00262339@setu.ie>
2024-11-20 09:42:49 +0000

    Fixing typo that prevented mesh creation

--- practical_07/mesh_script.py
+++ practical_07/mesh_script.py
@@ -1,5 +1,5 @@
 import bpy
 
-bpy.ops.primitive_cylinder.add(location=(0, 0, 0), vertices=12) # Create cylinder
+bpy.ops.mesh.primitive_cylinder_add(location=(0, 0, 0), vertices=12) # Create cylinder
 cylinder = bpy.context.object
 cylinder.scale (1, 1, 2)
