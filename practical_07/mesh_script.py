4c9ed5fc26d4641b3741cb507c1a641b4ebe58a4
LC-C00262339 <c00262339@setu.ie>
2024-11-20 09:46:53 +0000

    Fixed error where mesh scale was not called

--- practical_07/mesh_script.py
+++ practical_07/mesh_script.py
@@ -2,4 +2,4 @@ import bpy
 
 bpy.ops.mesh.primitive_cylinder_add(location=(0, 0, 0), vertices=12) # Create cylinder
 cylinder = bpy.context.object
-cylinder.scale (1, 1, 2)
+cylinder.scale = (1, 1, 2)
