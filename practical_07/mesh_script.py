56c322a61f55a3349148ebec080630253eaae942
LC-C00262339 <c00262339@setu.ie>
2024-11-21 10:13:47 +0000

    Changed mesh code to resemble building more

--- practical_07/mesh_script.py
+++ practical_07/mesh_script.py
@@ -1,8 +1,17 @@
 import bpy
 
+# Code should create a lighthouse
+
 bpy.ops.mesh.primitive_cylinder_add(location=(0, 0, 0), vertices=12) # Create cylinder
 cylinder = bpy.context.object
 cylinder.scale = (1, 1, 2)
 
 
-bpy.ops.mesh.primitive_cone_add(location=(0, 0, 4), vertices=12, radius1=1.5, radius2=0) # Create cone (roof)
+bpy.ops.mesh.primitive_cone_add(location=(0, 0, 3.49), vertices=12, radius1=1.2, radius2=0.12, depth=1) # Create cone (roof)
+cone = bpy.context.object
+
+bpy.ops.mesh.primitive_cylinder_add(location=(0, 0, 2.5), vertices=8, radius=0.9, depth=1) # Create octagonal prism using cylinder (inside)
+window = bpy.context.object
+
+# bpy.ops.mesh.primitive_cube_add(location=(0, 0, 3.5) # Create cube (inside)
+# cube = bpy.context.object
