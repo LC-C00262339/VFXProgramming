2d8619ecf39283ffc22e8577fbde09d83408cc23
LC-C00262339 <c00262339@setu.ie>
2024-11-25 13:39:01 +0000

    Added material for cylinder

--- practical_07/mesh_script.py
+++ practical_07/mesh_script.py
@@ -13,5 +13,13 @@ cone = bpy.context.object
 bpy.ops.mesh.primitive_cylinder_add(location=(0, 0, 2.5), vertices=8, radius=0.9, depth=1) # Create octagonal prism using cylinder (inside)
 window = bpy.context.object
 
+# Create object materials
+
+material = bpy.data.materials.new(name='lighthouse') # Lighthouse building exterior 
+# RGB = 1, 0.051, 0.040, 1
+material.diffuse_color = (1.0, 0.051, 0.040, 1)
+cylinder.data.materials.append(material) # Apply texture to cylinder
+
+
 # bpy.ops.mesh.primitive_cube_add(location=(0, 0, 3.5) # Create cube (inside)
 # cube = bpy.context.object
