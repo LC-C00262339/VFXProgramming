cadc3d2b10d3defda4901b8157c71d81cda1ae9b
LC-C00262339 <c00262339@setu.ie>
2024-11-27 10:18:31 +0000

    Adding modifier to fix code

--- practical_07/mesh_script.py
+++ practical_07/mesh_script.py
@@ -17,11 +17,17 @@ cone = bpy.context.object
 bpy.ops.mesh.primitive_cylinder_add(location=(0, 0, 2.5), vertices=8, radius=0.9, depth=1) # Create octagonal prism using cylinder (inside)
 window = bpy.context.object
 
+# Add ground
+
+bpy.ops.mesh.primitive_cube_add(location=(0, 0, -2.5), scale=(10, 10, 0.5))
+ground = bpy.context.object
+
 # Add a few rocks with randomized fractals for roughness
 
-bpy.ops.mesh.primitive_uv_sphere_add(location=(-1, 0, 0), segments=8, ring_count=8, radius=0.75)
+bpy.ops.mesh.primitive_uv_sphere_add(location=(-1, 0, 0), segments=6, ring_count=6, radius=0.75)
 rock = bpy.context.object
-rock.subdivide(number_cuts=1, fractal=3)
+rockmodif = rock.modifiers.new(name="Subdivision", type="SUBSURF")
+# rock.subdivide(number_cuts=1, fractal=3)
 
 # Create object materials
 
@@ -37,6 +43,11 @@ roof.metallic = 0.7 # Add metallic shine to roof
 roof.roughness = 0.75 # Affect how much light is diffused onto object
 
 cone.data.materials.append(roof) # Apply texture to roof
+
+grass = bpy.data.materials.new(name='grass') # Ground
+# RGB = 0.125, 0.7, 0.1, 1
+grass.diffuse_color = (0.125, 0.7, 0.1, 1)
+ground.data.materials.append(grass)
 
 # Add lighthouse light
 
