5f2a821c4cf0888bc8f5682aea6cec70f2773e01
(Student  C00262339) Leo Carabini <c00262339@setu.ie>
2024-12-01 23:41:52 +0000

    Added practical 10 files

---
+++ practical_10/nodes_script.py
@@ -0,0 +1,36 @@
+import bpy
+import os
+import sys
+import importlib
+import math
+
+
+nodes_name = 'GeometryNodes'
+
+object = bpy.context.object
+
+translate_obj = 'Translation'
+scale_obj = 'Scale'
+rotate_obj = 'Rotate'
+
+modifier = object.modifiers[nodes_name] # Modifier for nodes
+
+nodes = node_group.nodes
+
+# Create meshes
+
+bpy.ops.mesh.primitive_cube_add(location=(0, 4, 4))
+cube = bpy.context.object
+
+bpy.ops.mesh.primitive_uv_sphere_add(location=(0, 2, 2), segments=6, ring_count=6)
+sphere = bpy.context.objects
+
+for node in nodes:
+    if(node.label == translate_obj):
+        node.vector = (2.0, 2.0, 2.0)
+    if(node.label == rotate_obj):
+        node.rotation_euler=(0.0, 0.0, 0.0)
+    if(node.label == scale_obj)
+        node.vector = (1.0, 1.0, 1.0)
+
+object.data.update()
